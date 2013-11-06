8cbe6fefbc6dddc4a1e7d11be5d903500c2ddcb2
Robert <lemieszrobert@gmail.com>
2013-11-05 20:55:20 -0600

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+from TwitterAPI import TwitterAPI
+api = TwitterAPI("aiC1HsGnI81CrWQ78ejw","244t73B6eDybGbxPrqcxfXMjdfy3OBeqKndcnBakE5M","237561704-JIg6SthgLfZge8naa7Pun3ANpSo3r0BprtlrLFto","7FGLhL5UFW0F1RWRraF3HqPvY5cvhmOXMQt2FKjxC0")
+
+lon = -88.207270
+lat = 40.110588
+
+for i in range(100):
+	geocode = str(lat) + ',' + str(lon) + ',' + '100mi'
+	r = api.request('search/tweets', {'geocode' : geocode,'limit' : '100'})
+	for item in r.get_iterator():
+		print item['text']
+	lat+=4
+	lon+=4
